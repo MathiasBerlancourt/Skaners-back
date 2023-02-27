03cbb945708dcc425d54e40b6b982ee431ff8323
louislallier <louis.lallier@gmail.com>
2023-02-27 12:21:01 +0100

    initial commit on main

---
+++ api/routes/usersClient.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const { users } = require("../../models");
+console.log(users);
+router.get("/api/users", async (req, res) => {
+  try {
+    const usersList = await users.find();
+    res.status(200).json(usersList);
+  } catch (e) {
+    console.log(e);
+    return "Error";
+  }
+});
+
+module.exports = router;
