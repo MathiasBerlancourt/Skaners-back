2a7306de821580a4c2758f3ca9a4457c55dee275
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-02-27 18:25:30 +0100

    ajout de la route sneakers

---
+++ api/routes/sneakersRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const data = require("./sneakapi.json");
+const { sneakers } = require("../../models");
+
+router.get("/sneakers", async (req, res) => {
+  try {
+    const findSneaker = await sneakers.find();
+    res.json(findSneaker);
+  } catch (e) {
+    console.log(e);
+    return "Error";
+  }
+});
+
+router.get("/sneakers/:id", async (req, res) => {
+  try {
+    const findSneaker = await sneakers.findById(req.params.id);
+    res.json(findSneaker);
+  } catch (error) {
+    res.status(404).json({ error: "Bad request" });
+  }
+});
+
+module.exports = router;
