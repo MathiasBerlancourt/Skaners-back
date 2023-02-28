311fffdabcb836a1778a23cfd2b5202fbbaa0224
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-02-28 10:07:28 +0100

    fix server crash

--- api/routes/sneakersRoutes.js
+++ api/routes/sneakersRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const data = require("./sneakapi.json");
 const { sneakers } = require("../../models");
 
 router.get("/sneakers", async (req, res) => {
