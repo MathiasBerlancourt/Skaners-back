17730019979799e2c356161fe319e268151f4189
louislallier <louis.lallier@gmail.com>
2023-03-01 12:22:16 +0100

    /user/addSkan route OK /admin/allSkans route OK

--- api/routes/skansRoutes.js
+++ api/routes/skansRoutes.js
@@ -1,8 +1,14 @@
-const { skans } = require("../../models");
 const express = require("express");
 const router = express.Router();
+const skanController = require("../controllers/skanController");
+const fileUpload = require("express-fileupload");
 
-router.get("/add", async (req, res) => {});
+////// User add skan with his Id
+router.post("/user/addSkan", fileUpload(), skanController.addSkan);
+////// Admin allSkans
+router.get("/admin/allSkans", skanController.allSkans);
+////// Admin checkSkan isChecked -> true
+
 module.exports = router;
 
-//TODO add skans by user, Route admin allSkan, route checkSkan /:skanId, set true isChecked, then send it to the user with his id
+//TODO Route admin allSkan, route checkSkan /:skanId, set true isChecked, then send it to the user with his id
